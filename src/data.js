82fafe2c7a1451fded56c3b5a686a35147c70c23
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-08-27 22:35:02 +0100

    added to the code.

--- src/data.js
+++ src/data.js
@@ -1,32 +1,35 @@
 export default [
   {
+    title: "Punta Cana",
+    location: "Domincian Republic",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/23000+Punta+Cana,+Dominican+Republic/@18.6410566,-68.6372464,11z/data=!3m1!4b1!4m6!3m5!1s0x8ea891645dcbfe77:0x61881cfaed12f6f3!8m2!3d18.5600761!4d-68.372535!16zL20vMDVzYzdn?entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D",
+    startDate: "4 Jul, 2024",
+    endDate: "14 Jul, 2024",
+    description:
+      "Punta Cana, the easternmost tip of the Dominican Republic, abuts the Caribbean Sea and the Atlantic Ocean. It's a region known for its 32km stretch of beaches and clear waters. The Bávaro area and Punta Cana combine to form what's known as the Coconut Coast.",
+    imageUrl: "./assets/punta-cana.jpg",
+  },
+  {
     title: "Cologne Cathedral",
     location: "Germany",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Cologne+Cathedral/@50.9412784,6.9557065,17z/data=!3m1!4b1!4m6!3m5!1s0x47bf25a5369c3d2f:0x29ec913896e3a9c6!8m2!3d50.9412784!4d6.9582814!16zL20vMG00bmw?entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D",
+    startDate: "29 Mar, 2024",
+    endDate: "1 Apr, 2024",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "Cologne Cathedral is a cathedral in Cologne, North Rhine-Westphalia belonging to the Catholic Church. It is the seat of the Archbishop of Cologne and of the administration of the Archdiocese of Cologne.",
+    imageUrl: "./assets/cologne-cathedral.jpg",
   },
   {
-    title: "Cologne",
-    location: "Domincian Republic",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Buddha Tooth Relic Temple",
+    location: "Singapore",
+    googleMapsUrl:
+      "hhttps://www.google.com/maps/place/Buddha+Tooth+Relic+Temple/@1.281496,103.8416623,17z/data=!3m1!4b1!4m6!3m5!1s0x31da1972ce26bcd1:0x19202d08368ce010!8m2!3d1.2815155!4d103.8442397!16s%2Fm%2F0c3tv_j?entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D",
+    startDate: "29 Mar, 2023",
+    endDate: "7 Apr, 2023",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    title: "Cologne",
-    location: "Abu Dhabi",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "The Buddha Tooth Relic Temple and Museum is a Buddhist temple and museum complex located in the Chinatown district of Singapore. The temple's monastics and devotees officially practice Chinese Buddhism.",
+    imageUrl: "./assets/buddha-temple.jpg",
   },
 ];
